74f51ad3ffa960230d6dc606c9d81d806a743b51
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2023-06-04 19:11:19 +0300

    frontend changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
+import axios from 'axios';
 function App() {
+  const [fname,setFname]= useState("");
+  const[lname,setLname] = useState("");
+  const[student_data,setStudent_data] = useState({});
+ 
+  const handleSubmit = async (event) => {
+  event.preventDefault();
+  try{
+      await axios.post("http://localhost:5000/grades",{
+      firstname: fname,
+      lastname: lname
+      });
+      await axios.get("http://localhost:5000/grades").then(resp=> console.log(resp.data));
+  }
+  catch(error)
+  {
+    console.log(error);
+  }
+    
+      
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form onSubmit={handleSubmit}>
+        <h1>Register Page</h1>
+        <label>First Name </label>
+         <input type="text" name="fname"  placeholder='First Name ...' value={fname} onChange={(e) => setFname(e.target.value)} />
+         <label>Last Name </label>
+         <input type="text" name="lname" required placeholder='Last Name ...' value={lname} onChange={(e) => setLname(e.target.value)} />
+         <button onClick = {handleSubmit}> Next</button>
+       </form>
+       
+          
+        
+       
     </div>
   );
+  
 }
 
 export default App;
